c7ec1e21565eec0a00cf15d45b3fb999a728c0b0
helga-umrikh <zetaumr@gmail.com>
2023-06-17 20:41:40 +0300

    feat: add sign in form component

--- src/pages/Authorization/SignIn.js
+++ src/pages/Authorization/SignIn.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Authorization.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button } from 'react-bootstrap';
 
 function SignIn() {
+
+  //state variable for each input
+  const [login, setLogin] = useState('')
+  const [password, setPassword] = useState('')
+  const [isFormValid, setFormValidity] = useState(false)
+
+  const handleLoginChange = (event) => {
+    setLogin(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('Form submitted')
+  }
+
+  //check for values
+  const isInputValid = () => {
+    return login !== ''&& password !== ''
+  }
+
+  const handleFormValidity = () => {
+    setFormValidity(isInputValid())
+  }
+
   return (
-    <div>SignIn</div>
+    <div className='sign-in'>
+      <Form className='rounded p-4 p-sm-3' onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Логин или номер телефона:</Form.Label>
+          <Form.Control type="text" onChange={handleLoginChange} onBlur={handleFormValidity} />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Label>Пароль:</Form.Label>
+          <Form.Control type="password" onChange={handlePasswordChange} onBlur={handleFormValidity}/>
+        </Form.Group>
+        <Button variant='primary' type='submit' disabled={isFormValid}>Войти</Button>
+      </Form>
+    </div>
   )
 }
 
