76c9914da3446b2ee3a393c3692d0374c9c249de
helga-umrikh <zetaumr@gmail.com>
2023-06-18 23:45:54 +0300

    feat: add routing to authorizaion from main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Authorization from './pages/Authorization/Authorization';
-// import Main from './pages/Main';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Authorization from "./pages/Authorization/Authorization";
+import Main from "./pages/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Authorization />
+      <Routes>
+        <Route path="/" exact element={<Main />} />
+        <Route path="/authorization" exact element={<Authorization />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header/Register.js
+++ src/components/Header/Register.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Header.css';
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
     <div className="register">
       <button className="register__button">Зарегистрироваться</button>
       <div className="register__divider"></div>
-      <button className="register__enter-button">Войти</button>
+      <button className="register__enter-button"><Link to='/Authorization'>Войти</Link></button>
     </div>
   );
 }
